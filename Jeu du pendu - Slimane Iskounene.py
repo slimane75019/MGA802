da247a99bf32296281ad72d2539be6b40e19bf3a
slimane75019 <133153856+slimane75019@users.noreply.github.com>
2023-05-22 18:33:18 -0400

    Jeu du pendu - Slimane Iskounene.py

--- Jeu du pendu - Slimane Iskounene.py
+++ Jeu du pendu - Slimane Iskounene.py
@@ -1,11 +1,11 @@
 import random
 
-def choisir_mot():
+def choisir_mot():                                     #Cette fonction sélectionne un mot au hasard dans la listes de mots fournie
     with open("mots_pendu.txt", 'r') as f:
         mot = f.readlines()
     return random.choice(mot).strip()
 
-def affichage_mot(mot_a_deviner, lettres_donnees):
+def affichage_mot(mot_a_deviner, lettres_donnees):    #A chaque essai, le mot sera affichée avec les lettres trouvées et/ou des '_'
     mot_affiche=''
     for lettre in mot_a_deviner:
         if lettre in lettres_donnees:
@@ -22,22 +22,22 @@ def jeu_pendu():
     print("Bienvenue au jeu du pendu !")
     print(affichage_mot(mot_a_deviner, lettres_donnees))
 
-    while tentatives > 0:
+    while tentatives > 0:                                     #tant qu'il a encore des chances restantes, l'utilisateur peut donner une lettre
         lettre = input("Entrez une lettre : \n")
 
-        if lettre in lettres_donnees:
+        if lettre in lettres_donnees:                         
             print("Vous avez déjà deviné cette lettre.")
 
-        elif lettre in mot_a_deviner:
+        elif lettre in mot_a_deviner:                         #si la lettre donnée est dans le mot, elle sera ajouté au mot affiché et remplacera un des '_'
             lettres_donnees.append(lettre)
             mot_affiche = affichage_mot(mot_a_deviner, lettres_donnees)
             print(mot_affiche)
 
-            if "_" not in mot_affiche:
+            if "_" not in mot_affiche:                         #si toutes les lettres ont été devinées, l'utilisateur a gagné
                 print("Félicitation ! Vous avez deviné le mot secret :", mot_a_deviner)
                 return
 
-        else:
+        else:                                                  #si la lettre n'est pas dans le mots, l'utilisateur perd une chance
             tentatives -= 1
             print("La lettre", lettre, "n'est pas dans le mot.")
             print("Tentatives restantes :", tentatives)
